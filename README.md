d3db8559e7c33dfcbc24d2b8aeefcf7ee24d0431
Edward <50692450+e19931107@users.noreply.github.com>
2024-01-09 22:23:42 +0800

    Finish Momo part and edit README file

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
-# Momo, Pchome, 原價屋爬蟲
+# Momo, Pchome, Coolpc(原價屋)爬蟲
+
 ## 目的
+
 因為前一份工作需要常常check競爭對手的規格、價格、通路產品（看有什麼產品該通路特有）等
 因此常常需要將網站的資訊複製下來變成ppt檔
 但這一來一回當中花了非常多時間
 
-現在藉由這個機會，運用Python來快速獲取網站上的資料
-減少大量的時間，以增加工作效率
+現在藉由這個機會，運用Python來快速獲取網站上的資料，以增加工作效率
 
 ## 運用工具
-Selenium
+
+Selenium, Beautifulsoup, requests
+
+## 實際成果
+
+Momo:
+
+![1704810079232](image/README/1704810079232.png)
+
+Pchome:
+
+![1704810022745](image/README/1704810022745.png)
+
+Coolpc:
+
+![1704809939048](image/README/1704809939048.png)
+
+## 困難點
+
+Momo的部分因為桌面板的html較不易爬取，因此採用行動版的部分來爬取
+
+故需要再加上一行header
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
--- Website/Momo.py
+++ Website/Momo.py
@@ -1,21 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
+def momo():
+    import requests
+    from bs4 import BeautifulSoup
 
-brand_url = {'asus': 'https://m.momoshop.com.tw/category.momo?cn=4300100008&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6',
-             'ROG': 'https://m.momoshop.com.tw/category.momo?cn=4300100673&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6',
-             'acer': 'https://m.momoshop.com.tw/category.momo?cn=4300100003&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6',
-             'Lenovo': 'https://m.momoshop.com.tw/category.momo?cn=4300100160&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6',
-             'ThinkPad': 'https://m.momoshop.com.tw/category.momo?cn=4300100179&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6',
-             'HP': 'https://m.momoshop.com.tw/category.momo?cn=4300100013&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6',
-             'Surface': 'https://m.momoshop.com.tw/category.momo?cn=4300100759&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6',
-             'MSI': 'https://m.momoshop.com.tw/category.momo?cn=4300100239&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6',
-             'DELL': 'https://m.momoshop.com.tw/category.momo?cn=4300100202&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6',
-             'Gigabyte': 'https://m.momoshop.com.tw/category.momo?cn=4300100446&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6'}
+    brand_url = {'asus': '4300100008', 'ROG': '4300100673', 'acer': '4300100003', 'Lenovo': '4300100160',
+                'ThinkPad': '4300100179', 'HP': '4300100013', 'Surface': '4300100759',
+                'MSI': '4300100239', 'DELL': '4300100202', 'Gigabyte': '4300100446'}
 
-url = 'https://m.momoshop.com.tw/category.momo?cn=4300100446&cateLevel=2&sourcePageType=4&imgSH=itemizedType&sortType=6'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
 
-re = requests.get(url)
-momo = BeautifulSoup(re.text, "html.parser")
+    for brand, code in brand_url.items():
+        i = 1
+        while True:
+            try:
+                head_url = 'https://m.momoshop.com.tw/category.momo?cn='
+                end_url = f'&page={i}'
+                complete_url = head_url+code+end_url
+                re = requests.get(complete_url, headers=headers)
+                momo = BeautifulSoup(re.text, 'lxml')
 
-all_momo= momo.select('li')
-print(all_momo)
+                product_name = momo.select('h3[class="prdName"]')
+                product_price = momo.select('b[class="price"]')
+                if product_name and product_price:
+                    for name, price in zip(product_name[:-1], product_price[:-1]):
+                        print(name.text,'/',price.text)
+                else:
+                    break
+                i += 1
+            except:
+                break
